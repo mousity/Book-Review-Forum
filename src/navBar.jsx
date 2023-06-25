48682ef4032de05391f865800088d2995493d16c
Samr Mouna <samr.mouna@gmail.com>
2023-06-25 01:10:01 -0400

    Added logo child to title parent node

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar () {
     return (
         <div className="entirePage">
             <span className="heading">
-                <img className="logo" src="src\assets\Logo.jpg" alt="Book Cat" ></img>
+                <img className="logo" src="src\assets\Logo.jpg" alt="Book Cat"></img>
                 <h1 className="topTitle">Path of Pages Forum</h1>
             </span>
             <div className="page">
