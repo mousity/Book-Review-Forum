2ff6228050ce0e02bb8bb88132171d01e6c6a991
Samr Mouna <samr.mouna@gmail.com>
2023-06-26 16:54:52 -0400

    lifted a second state to filter the incoming list

--- src/bookCard.jsx
+++ src/bookCard.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
-export default function BookCard ({books, query}) {
+export default function BookCard ({books, titleQuery, authorQuery}) {
     
     const list = [];
-    console.log(query);
 
   books.forEach((item, index) => {
 
-    if(item.title.toLowerCase().includes(query.toLowerCase())){
+    if(item.title.toLowerCase().includes(titleQuery.toLowerCase()) && item.author.toLowerCase().includes(authorQuery.toLowerCase())){
       list.push(
         <div className="card" key={index}>
           <img className="cardImage" src={item.image.src} alt={item.image.alt}/>
           <div className="cardTitle">{item.title}</div>
+          <div className="author">By {item.author}</div>
           <div className="cardDesc">{item.description}</div>
           <div className="options">
               <button className="favorite">Make Favorite?</button>
