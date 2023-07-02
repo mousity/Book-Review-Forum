8a6c7e88151bfaef75b1d22808cbe1888816af29
Samr Mouna <samr.mouna@gmail.com>
2023-07-02 18:02:21 -0400

    added libraries

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <PageContent />
       },
+      {
+        path: "/books/:id",
+        element: <PageContent />
+      },
     ],
   },
 ]);
